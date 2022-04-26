a531ab052fedb7c6073bf30a9242b3068820134d
dudckd6744 <dudckd6744@naver.com>
2022-04-26 22:22:03 +0900

    22-04-26 GPRC 기본 셋팅

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+import { join } from 'path';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.GRPC,
+      options: {
+        package: 'person',
+        protoPath: join(__dirname, '../proto', 'person.proto'),
+      },
+    },
+  );
+
+  await app.listen();
 }
 bootstrap();
